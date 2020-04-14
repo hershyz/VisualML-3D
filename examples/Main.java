f1bf420f70e1fb8e0a1954dab8a66dc95605760a
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2020-04-14 13:30:10 -0400

    Update Main.java

--- examples/Main.java
+++ examples/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        visualml3d vml3d = new visualml3d("c:/users/bagal/desktop/data.txt");
+        visualml3d vml3d = new visualml3d("data.txt");
         vml3d.init();
         vml3d.printSummary();
         String predictedCategory = vml3d.predictCategory(20, 20, 20);
